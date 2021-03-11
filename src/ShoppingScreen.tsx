8e96fbbc60d6d5cb171d359ca6eed4fe6f5edf35
Cheryl Laird <claird@hublsoft.com>
2021-03-11 16:18:04 +0000

    Adding placeholder pages

--- src/ShoppingScreen.tsx
+++ src/ShoppingScreen.tsx
@@ -3,21 +3,36 @@ import {
     StyleSheet,
     View,
     Text,
+    Image,
 } from "react-native";
 
 const styles = StyleSheet.create({
-    container: {
-        alignItems: "center",
-        flex: 1,
-        justifyContent: "center",
-        padding: 20,
+    image: {
+        width: 250,
+        height: 250,
+        maxWidth: "100%",
     },
+    placeholder: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    placeholderText: {
+        fontSize: 16,
+        color: "grey",
+        marginBottom: 40,
+    }
 });
 
 export function ShoppingScreen(): React.ReactElement {
     return (
-        <View style={styles.container}>
-            <Text>Coming soon</Text>
+        <View style={styles.placeholder}>
+            <Image
+                style={styles.image}
+                source={require("../assets/shopping.png")}
+            />
+            <Text style={styles.placeholderText}>Just running to the shops...</Text>
         </View>
     );
 }
--- src/recipe/RecipeListScreen.tsx
+++ src/recipe/RecipeListScreen.tsx
@@ -1,12 +1,48 @@
 import React from "react";
 import {
     FlatList,
+    Image,
+    Text,
+    View,
+    StyleSheet,
 } from "react-native";
 import { RecipeSummary } from "./RecipeSummary";
 import { useRecipes } from "./useRecipes";
 
+const styles = StyleSheet.create({
+    image: {
+        width: 250,
+        height: 250,
+        maxWidth: "100%",
+    },
+    placeholder: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    placeholderText: {
+        fontSize: 16,
+        color: "grey",
+        marginBottom: 40,
+    }
+});
+
 export function RecipeListScreen(): React.ReactElement {
     const { recipes } = useRecipes();
+
+    if (!Object.keys(recipes).length) {
+        return (
+            <View style={styles.placeholder}>
+                <Image
+                    style={styles.image}
+                    source={require("../../assets/search.png")}
+                />
+                <Text style={styles.placeholderText}>Oops, it is looking a little empty here.</Text>
+            </View>
+        )
+    }
+
     const recipesWithId = Object.keys(recipes).map((id) => ({ ...recipes[id], id }));
 
     return (
